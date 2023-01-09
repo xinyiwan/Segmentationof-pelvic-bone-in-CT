efb9c6a2ff7508395e4313e6228cbdea1310f9e8
Xinyi Wan <wanxyzju@163.com>
2023-01-09 15:02:37 +0100

    Update results

--- utils/classification.py
+++ utils/classification.py
@@ -4,68 +4,144 @@ from tensorflow.keras.layers import Dense, Flatten, Activation, Dropout
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, SpatialDropout2D
 from tensorflow.keras.optimizers import Adam, RMSprop
 import numpy as np
+import matplotlib.pyplot as plt
 
-def model(img_ch, img_width, img_height, n_base):
-    
+def AlexNet(img_ch, img_width, img_height, n_base=8, dropout=True, batch_norm=True):
+
+    """
+    This function is used to resample the input image mask with the given size.
+
+    Parameters
+    ----------
+    img_ch : 
+        The number of image chanels
+    img_width :
+        Image width
+    img_height :
+        Image height
+    n_base :
+        Number of neurons in the first convolution layer
+    dropout : True or False
+        Include dropout layers
+    batch_norm : True or False
+        Include batch normalization layers
+
+    Returns
+    -----------
+    The model ready for the training.
+
+    """
+
     model = Sequential()
-    
+
     model.add(Conv2D(filters=n_base, input_shape=(img_width, img_height, img_ch),
-                     kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
+    kernel_size=(3,3), strides=(1,1), padding='same'))
+    if batch_norm:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
-
-    model.add(Conv2D(filters=n_base*2, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
+   
+    model.add(Conv2D(filters= n_base *2, kernel_size=(3,3), strides=(1,1), padding='same'))
+    if batch_norm:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
-
-    model.add(Conv2D(filters=n_base*4, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
+    
+    model.add(Conv2D(filters= n_base *4, kernel_size=(3,3), strides=(1,1), padding='same'))
+    if batch_norm:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
-
-    model.add(Conv2D(filters=n_base*4, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
+    
+    model.add(Conv2D(filters= n_base *4, kernel_size=(3,3), strides=(1,1), padding='same'))
+    if batch_norm:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
-
-    model.add(Conv2D(filters=n_base*2, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
+    
+    model.add(Conv2D(filters= n_base *2, kernel_size=(3,3), strides=(1,1), padding='same', name = 'Last_ConvLayer'))
+    if batch_norm:
+        model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
-
+    
     model.add(Flatten())
-    model.add(Dense(64))
+    model.add(Dense(128))
+    if dropout:
+        model.add(Dropout(0.4))
     model.add(Activation('relu'))
-    model.add(Dropout(0.1))
-
     model.add(Dense(64))
+    if dropout:
+        model.add(Dropout(0.4))
     model.add(Activation('relu'))
-    model.add(Dropout(0.1))
 
-    model.add(Dense(2))
-    model.add(Activation('softmax'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.summary()
 
-    model.summary()   
+    model.compile(loss = 'binary_crossentropy',         
+            optimizer = Adam(lr = 1e-5),
+            metrics = ['accuracy'])
+
     return model
 
-def train_classifier(train_img, train_labels, val_img, val_labels, learning_rate, n_epochs): 
-    """
-    Receive a list of images `im_list` and a list of vectors (one per image) 
-    with the labels 0 or 1 depending on the sagittal 2D slice contains or not 
-    the obturator foramen. Returns the trained classifier.
-    """ 
+def load_train_data(imgs):
+    # imgs should be in lists of 53,54,55
+    x_train = []
+    y_train = []
+    for img in imgs:
+        for idx in range(512):
+            slice = img[:,:,idx]
+            x_train.append(slice)
+    x_train = np.array(x_train)
 
-    batch_size = 8
-    n_base = 8
-    MLP = model(1, 256, 512, n_base)
-    MLP.compile(loss = 'categorical_crossentropy',         
-              optimizer = Adam(lr = learning_rate),
-              metrics = ['accuracy'])
+    # Generate ground truth for training data
+    y_53 = np.zeros(512)
+    y_53[173:212] = 1
+    y_53[312:345] = 1
+
+    y_54 = np.zeros(512)
+    y_54[160:203] = 1
+    y_54[290:326] = 1
+
+    y_55 = np.zeros(512)
+    y_55[181:225] = 1
+    y_55[292:331] = 1
+
+    y_train = np.concatenate((y_53, y_54, y_55),axis=0)
+
+    return x_train, y_train
     
-    history = MLP.fit(train_img, train_labels, 
-                      batch_size = batch_size,
-                      validation_data = (val_img, val_labels),
-                      epochs = n_epochs, verbose=1)
-    
-    return MLP, history
+def load_test_data(img):
+    x_test = []
+    for idx in range(512):
+        slice = img[:,:,idx]
+        x_test.append(slice)
+    x_test = np.array(x_test)
+    return x_test
+
+def plot_history(history):
+    plt.figure(figsize=(4, 4))
+    plt.title("Learning curve")
+    plt.plot(history.history["loss"], label="loss")
+    plt.plot(history.history["val_loss"], label="val_loss")
+    plt.plot( np.argmin(history.history["val_loss"]),
+            np.min(history.history["val_loss"]),
+            marker="x", color="r", label="best model")
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss Value")
+    plt.legend()
+    plt.show()
+
+    plt.figure(figsize=(4, 4))
+    plt.title("Accuracy")
+    plt.plot(history.history["accuracy"], label="accuracy")
+    plt.plot(history.history["val_accuracy"], label="val_accuracy")
+    plt.plot( np.argmax(history.history["val_accuracy"]),
+            np.max(history.history["val_accuracy"]),
+            marker="x", color="r", label="best model")
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
 
